86ffb2044a0e465d46790a48dfb2fd1b52accf9f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-07 06:28:13 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -1,5 +1,5 @@
 class CreateNetworks < ActiveRecord::Migration[4.2]
-  #define a change method in which to do the migration
+
   def change
     create_table :networks do |t| #we get a block variable here for the table
       #primary key of :id is created for us!
