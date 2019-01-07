c44081819436cbc94073d6d5195aedb881f54385
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-07 06:39:12 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,12 @@
 
 class CreateActors < ActiveRecord::Migrations[4.2]
   
-  
+  def change
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
+  end
   
   
   
