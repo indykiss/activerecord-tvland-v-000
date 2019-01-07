3c54b9e44c22a5f7db53ccac66d276c6e3670b5b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-07 07:00:19 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
-   def list_roles
+  def list_roles
     self.characters.collect do |character| 
       "#{character.name} - #{character.show.name}"
     end
